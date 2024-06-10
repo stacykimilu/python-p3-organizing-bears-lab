35f65dd1dc8c73e443ce119eea4b2152f3fdccd6
Stacy <stacykimilu@gmail.com>
2024-06-11 01:44:12 +0300

    completed lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+('Rowdy', 3, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 2, 'F', 'Gray', 'Calm', 1),
+('Sergeant Brown', 7, 'M', 'Brown', 'Strict', 1),
+('Melissa', 4, 'F', 'White', 'Playful', 1),
+('Grinch', 10, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 6, 'F', 'Brown', 'Shy', 1),
+(NULL, NULL, 'F', NULL, NULL, 1);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,35 @@
+# select_all_female_bears_return_name_and_age = """
+#     Write your SQL query here
+# """
+
+# select_all_bears_names_and_orders_in_alphabetical_order = """
+#     Write your SQL query here
+# """
+
+# select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
+#     Write your SQL query here
+# """
+
+# select_oldest_bear_and_returns_name_and_age = """
+#     Write your SQL query here
+# """
+# select_youngest_bear_and_returns_name_and_age = """
+#     Write your SQL query here
+# """
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+    SELECT
+        name,
+        age
+    FROM bears
+    WHERE sex='F';
 """
 
-select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+select_all_alive_bears_return_name_and_color = """
+    SELECT
+        name,
+        color
+    FROM bears
+    WHERE alive=1;
 """
 
-select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
-"""
-
-select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
-select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
-"""
+# Add other required queries here
